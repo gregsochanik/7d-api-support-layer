f7e82c032dc1b42ef5f58ac74cded2a55db8adb3
greg Sochanik <greg@sochanik.com>
2013-03-07 15:07:27 +0000

    BUYNOW-39: Make sure that only free tracks are supported not priced tracks or releases

--- src/SevenDigital.ApiInt.ServiceStack.Unit.Tests/Services/FreePurchaseServiceTests.cs
+++ src/SevenDigital.ApiInt.ServiceStack.Unit.Tests/Services/FreePurchaseServiceTests.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using NUnit.Framework;
 using Rhino.Mocks;
+using ServiceStack.Common.Web;
 using SevenDigital.Api.Schema.LockerEndpoint;
 using SevenDigital.Api.Schema.OAuth;
+using SevenDigital.Api.Schema.Pricing;
+using SevenDigital.Api.Schema.TrackEndpoint;
+using SevenDigital.ApiInt.Catalogue;
 using SevenDigital.ApiInt.Mapping;
 using SevenDigital.ApiInt.Model;
 using SevenDigital.ApiInt.Purchasing;
@@ -15,27 +20,45 @@ namespace SevenDigital.ApiInt.ServiceStack.Unit.Tests.Services
 	[TestFixture]
 	public class FreePurchaseServiceTests
 	{
+		private const int PRICED_TRACK_ID = 1234;
+		private IItemBuyer _itemBuyer;
+		private IPurchaseItemMapper _mapper;
+		private ICatalogue _catalogue;
+		private static readonly Track _freeTrack = new Track { Price = new Price { Value = "0" } };
+		private static readonly Track _pricedTrack = new Track { Id = PRICED_TRACK_ID, Price = new Price() };
+		private PurchasedItem _expectedPurchasedItem;
+		private List<LockerRelease> _expectedLockerReleases;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_itemBuyer = MockRepository.GenerateStub<IItemBuyer>();
+			_mapper = MockRepository.GenerateStub<IPurchaseItemMapper>();
+
+			_expectedLockerReleases = new List<LockerRelease>();
+			_itemBuyer.Stub(x => x.BuyItem(null, null)).IgnoreArguments().Return(_expectedLockerReleases);
+
+			_expectedPurchasedItem = new PurchasedItem();
+			_mapper.Stub(x => x.Map(null, null)).IgnoreArguments().Return(_expectedPurchasedItem);
+
+			_catalogue = MockRepository.GenerateStub<ICatalogue>();
+			_catalogue.Stub(x => x.GetATrackWithPrice(null, 0)).IgnoreArguments().Return(_freeTrack);
+			_catalogue.Stub(x => x.GetATrackWithPrice("GB",PRICED_TRACK_ID)).Return(_pricedTrack);
+		}
+
 		[Test]
 		public void Adds_item_to_locker()
 		{
-			var itemBuyer = MockRepository.GenerateStub<IItemBuyer>();
-			var expectedLockerReleases = new List<LockerRelease>();
-			itemBuyer.Stub(x => x.BuyItem(null, null)).IgnoreArguments().Return(expectedLockerReleases);
-
-			var mapper = MockRepository.GenerateStub<IPurchaseItemMapper>();
-			var expectedPurchasedItem = new PurchasedItem();
-			mapper.Stub(x => x.Map(null, null)).IgnoreArguments().Return(expectedPurchasedItem);
-
-			var freePurchaseService = new FreePurchaseService(mapper, itemBuyer)
+			var freePurchaseService = new FreePurchaseService(_mapper, _itemBuyer, _catalogue)
 			{
 				RequestContext = ContextHelper.LoggedInContext()
 			}; 
 			var freePurchaseRequest = new FreePurchaseRequest();
 			var freePurchaseResponse = freePurchaseService.Get(freePurchaseRequest);
 
-			itemBuyer.AssertWasCalled(x=>x.BuyItem(Arg<ItemRequest>.Is.Equal(freePurchaseRequest), Arg<OAuthAccessToken>.Is.Anything));
+			_itemBuyer.AssertWasCalled(x => x.BuyItem(Arg<ItemRequest>.Is.Equal(freePurchaseRequest), Arg<OAuthAccessToken>.Is.Anything));
 
-			Assert.That(freePurchaseResponse.Item, Is.EqualTo(expectedPurchasedItem));
+			Assert.That(freePurchaseResponse.Item, Is.EqualTo(_expectedPurchasedItem));
 			Assert.That(freePurchaseResponse.OriginalRequest, Is.EqualTo(freePurchaseRequest));
 			Assert.That(freePurchaseResponse.Status.IsSuccess);
 		}
@@ -43,11 +66,7 @@ namespace SevenDigital.ApiInt.ServiceStack.Unit.Tests.Services
 		[Test]
 		public void Uses_oauth_criteria()
 		{
-			var itemBuyer = MockRepository.GenerateStub<IItemBuyer>();
-			itemBuyer.Stub(x => x.BuyItem(null, null)).IgnoreArguments().Return(new List<LockerRelease>());
-			var mapper = MockRepository.GenerateStub<IPurchaseItemMapper>();
-
-			var freePurchaseService = new FreePurchaseService(mapper, itemBuyer)
+			var freePurchaseService = new FreePurchaseService(_mapper, _itemBuyer, _catalogue)
 			{
 				RequestContext = ContextHelper.LoggedInContext()
 			};
@@ -55,10 +74,46 @@ namespace SevenDigital.ApiInt.ServiceStack.Unit.Tests.Services
 
 			freePurchaseService.Get(freePurchaseRequest);
 
-			itemBuyer.AssertWasCalled(x => x.BuyItem(Arg<ItemRequest>.Is.Equal(freePurchaseRequest),
-				Arg<OAuthAccessToken>.Matches(accessToken => accessToken.Token == FakeUserData.FakeAccessToken.Token && accessToken.Secret == FakeUserData.FakeAccessToken.Secret)));
+			_itemBuyer.AssertWasCalled(x => x.BuyItem(
+				Arg<ItemRequest>.Is.Equal(freePurchaseRequest),
+				Arg<OAuthAccessToken>.Matches(accessToken => accessToken.Token == FakeUserData.FakeAccessToken.Token && 
+					accessToken.Secret == FakeUserData.FakeAccessToken.Secret))
+					);
 		}
 
-		
+		[Test]
+		public void Throws_error_if_item_is_not_free()
+		{
+			var catalogue = MockRepository.GenerateStub<ICatalogue>();
+			catalogue.Stub(x => x.GetATrackWithPrice(null, 0)).IgnoreArguments().Return(_pricedTrack);
+
+			var freePurchaseService = new FreePurchaseService(_mapper, _itemBuyer, catalogue)
+			{
+				RequestContext = ContextHelper.LoggedInContext()
+			};
+			var freePurchaseRequest = new FreePurchaseRequest{CountryCode = "GB", Id=PRICED_TRACK_ID, Type = PurchaseType.track};
+
+			var httpError = Assert.Throws<HttpError>(() => freePurchaseService.Get(freePurchaseRequest));
+			Assert.That(httpError.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+			Assert.That(httpError.Message, Is.EqualTo(string.Format("This track is not free! {0}", _pricedTrack.Price.Status)));
+			Assert.That(httpError.ErrorCode, Is.EqualTo("TrackNotFree"));
+		}
+
+		[Test]
+		public void Throws_error_if_item_is_release()
+		{
+			var catalogue = MockRepository.GenerateStub<ICatalogue>();
+
+			var freePurchaseService = new FreePurchaseService(_mapper, _itemBuyer, catalogue)
+			{
+				RequestContext = ContextHelper.LoggedInContext()
+			};
+			var freePurchaseRequest = new FreePurchaseRequest {Type = PurchaseType.release };
+
+			var httpError = Assert.Throws<HttpError>(() => freePurchaseService.Get(freePurchaseRequest));
+			Assert.That(httpError.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+			Assert.That(httpError.Message, Is.EqualTo("You cannot access releases for free - only tracks are currently supported"));
+			Assert.That(httpError.ErrorCode, Is.EqualTo("ReleasesNotSupported"));
+		}
 	}
 }
--- src/SevenDigital.ApiInt.ServiceStack/Services/FreePurchaseService.cs
+++ src/SevenDigital.ApiInt.ServiceStack/Services/FreePurchaseService.cs
@@ -1,5 +1,9 @@
 using System.Linq;
+using System.Net;
+using ServiceStack.Common.Web;
 using ServiceStack.ServiceInterface;
+using SevenDigital.Api.Schema.Pricing;
+using SevenDigital.ApiInt.Catalogue;
 using SevenDigital.ApiInt.Mapping;
 using SevenDigital.ApiInt.Model;
 using SevenDigital.ApiInt.Purchasing;
@@ -11,15 +15,20 @@ namespace SevenDigital.ApiInt.ServiceStack.Services
 	{
 		private readonly IPurchaseItemMapper _mapper;
 		private readonly IItemBuyer _itemBuyer;
+		private readonly ICatalogue _catalogue;
 
-		public FreePurchaseService(IPurchaseItemMapper mapper, IItemBuyer itemBuyer)
+		public FreePurchaseService(IPurchaseItemMapper mapper, IItemBuyer itemBuyer, ICatalogue catalogue)
 		{
 			_mapper = mapper;
 			_itemBuyer = itemBuyer;
+			_catalogue = catalogue;
 		}
 
 		public FreePurchaseResponse Get(FreePurchaseRequest request)
 		{
+			AssertItemIsTrack(request);
+			AssertItemIsFree(request);
+
 			var accessToken = this.TryGetOAuthAccessToken();
 			var lockerReleases = _itemBuyer.BuyItem(request, accessToken).ToList();
 
@@ -29,6 +38,27 @@ namespace SevenDigital.ApiInt.ServiceStack.Services
 				OriginalRequest = request,
 				Status = new PurchaseStatus(true, "OK", lockerReleases)
 			};
+		}
+
+		private static void AssertItemIsTrack(FreePurchaseRequest request)
+		{
+			if (request.Type == PurchaseType.release)
+			{
+				throw new HttpError(HttpStatusCode.Forbidden,
+				                    "ReleasesNotSupported",
+				                    "You cannot access releases for free - only tracks are currently supported");
+			}
+		}
+
+		private void AssertItemIsFree(ItemRequest request)
+		{
+			var aTrackWithPrice = _catalogue.GetATrackWithPrice(request.CountryCode, request.Id);
+			if (aTrackWithPrice.Price.Status != PriceStatus.Free)
+			{
+				throw new HttpError(HttpStatusCode.Forbidden,
+				                    "TrackNotFree",
+				                    string.Format("This track is not free! {0}", aTrackWithPrice.Price.Status));
+			}
 		}
 	}
 }
