7112648470a2b22a20f5d276f852c31eef71938b
greg Sochanik <greg@sochanik.com>
2013-11-15 17:35:10 +0000

    Moved baskethandler code acros to buynow widget

---
+++ src/SevenDigital.ApiSupportLayer/Basket/PurchaseData.cs
@@ -0,0 +1,9 @@
+namespace SevenDigital.ApiSupportLayer.Basket
+{
+	public class PurchaseData
+	{
+		public string CountryCode { get; set; }
+		public string SalesTagName { get; set; }
+		public string SalesTagValue { get; set; }
+	}
+}
