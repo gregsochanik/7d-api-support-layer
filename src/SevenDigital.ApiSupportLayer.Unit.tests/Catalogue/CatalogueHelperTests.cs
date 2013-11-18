209147d03eac24d22cab7cafff06326536a81e48
greg Sochanik <greg@sochanik.com>
2013-11-18 11:27:25 +0000

    Added tests around CatalogueHelper

---
+++ src/SevenDigital.ApiSupportLayer.Unit.tests/Catalogue/CatalogueHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SevenDigital.ApiSupportLayer.Catalogue;
+
+namespace SevenDigital.ApiSupportLayer.Unit.Tests.Catalogue
+{
+	[TestFixture]
+	public class CatalogueHelperTests
+	{
+		[Test]
+		public void TestName()
+		{
+			var countriesToCheckInCatalogue = CatalogueHelper.CountriesToCheckInCatalogue;
+
+			var expectedCountries = new[] {"GB", "US", "DE", "FR"};
+			var enumerator = countriesToCheckInCatalogue.GetEnumerator();
+
+			foreach (var expectedCountry in expectedCountries)
+			{
+				enumerator.MoveNext();
+				Assert.That(enumerator.Current, Is.EqualTo(expectedCountry));
+			}
+		}
+	}
+}
